92218996ef836a78ee3275cce01d27d02f7e13e8
Ubuntu <vaders@VadersAppDevelopmentVM.xvntebwtvllejabvnrdnlkow1b.bx.internal.cloudapp.net>
2017-02-27 04:12:31 +0000

    Update port for 80

--- runserver.py
+++ runserver.py
@@ -8,9 +8,9 @@ from InventoryManagement import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 5555
+        PORT = 80
     app.debug=True
     app.run(HOST, PORT)
 
