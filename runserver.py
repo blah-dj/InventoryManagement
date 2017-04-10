9e12ede90cc7180ef505be275e2e2148b7ef4513
Ubuntu <vaders@VadersAppDevelopmentVM.xvntebwtvllejabvnrdnlkow1b.bx.internal.cloudapp.net>
2017-04-10 02:38:29 +0000

    Updates

--- runserver.py
+++ runserver.py
@@ -6,7 +6,7 @@ from os import environ
 from InventoryManagement import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '')
     try:
         PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
--- views.py
+++ views.py
@@ -5,7 +5,7 @@ Routes and views for the flask application.
 import pyodbc
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from InventoryManagement import app
 
 @app.route('/')
@@ -23,6 +23,13 @@ def display_category():
         'category.html', category=get_category()
     )
 
+@app.route('/update_inventory')
+def update_category():
+    """Displays inventory"""
+    return render_template(
+        'update_category.html', category=get_category()
+    )
+
 def get_category():
     server = 'home-stuff.database.windows.net'
     database = 'TheVadersInventoryManagement'
@@ -35,9 +42,87 @@ def get_category():
     categories = record.fetchall()   
     return categories
 
-@app.route('/display')
+@app.route('/display/', methods=['POST', 'GET'])
 def display_inventory():
     """Displays inventory"""
+    if request.method == 'POST':
+        category = str(request.form['getinventoryfor'])
+    if request.method == 'GET':
+        category = str(request.args.get('getinventoryfor'))
+        '''
+        category = str(request.query_string)
+        import pdb;pdb.set_trace()
+        (Pdb) category
+        'getinventoryfor=Kitchen'
+        (Pdb) type(category)
+        <type 'str'>
+        (Pdb) category.split('=')
+        ['getinventoryfor', 'Kitchen']
+        (Pdb) category.split('=')[1]
+        'Kitchen'
+        (Pdb)
+        '''
     return render_template(
-        'category.html', category=get_category()
+        'category_state.html', category=getinventorystatus(category)
     )
+
+@app.route('/update/', methods=['POST', 'GET'])
+def update_inventory():
+    """Displays inventory"""
+    if request.method == 'POST':
+        category = str(request.form['getinventoryfor'])
+    if request.method == 'GET':
+        category = str(request.args.get('getinventoryfor'))
+        '''
+        category = str(request.query_string)
+        import pdb;pdb.set_trace()
+        (Pdb) category
+        'getinventoryfor=Kitchen'
+        (Pdb) type(category)
+        <type 'str'>
+        (Pdb) category.split('=')
+        ['getinventoryfor', 'Kitchen']
+        (Pdb) category.split('=')[1]
+        'Kitchen'
+        (Pdb)
+        '''
+    return render_template(
+        'inventory_state.html', category=getinventorystatus(category)
+    )
+
+def getinventorystatus(category):
+    server = 'home-stuff.database.windows.net'
+    database = 'TheVadersInventoryManagement'
+    username = 'thevaders'
+    password = 'Zomb!e1421'
+    driver= '{ODBC Driver 13 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    record = cursor.execute("select items.name, items.quantity, categories.category from items INNER JOIN categories ON categories.id=items.category and categories.category='%s'" % category)
+    categories = record.fetchall()
+    return categories
+
+@app.route('/add_item/')
+def update_item():
+    """Displays inventory"""
+    item_list = str(request.args.get('item'))
+    item = item_list.split('_')[0].replace("+", " ")
+    if len(item_list.split('_')) == 3:
+        category = item_list.split('_')[1].replace("+", " ")
+        quantity = int(item_list.split('_')[2].replace("+", " "))
+        return render_template('update_existing_item.html', item=item,
+                               category=category, quantity=quantity)
+    if item == "Add New Item":
+        return render_template('add_new_item.html')
+
+def update_database(category, item, quantity):
+    server = 'home-stuff.database.windows.net'
+    database = 'TheVadersInventoryManagement'
+    username = 'thevaders'
+    password = 'Zomb!e1421'
+    driver= '{ODBC Driver 13 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    record = cursor.execute("select items.name, items.quantity, categories.category from items INNER JOIN categories ON categories.id=items.category and categories.category='%s'" % category)
+    categories = record.fetchall()
+    return categories
